9c8f5408b04d73cd46f0415c23eb5facd96c278e
efrilm <efril.mulanda@gmail.com>
2025-08-28 17:51:46 +0700

    feat: splash page

--- lib/common/theme/app_color.dart
+++ lib/common/theme/app_color.dart
@@ -1,5 +1,14 @@
 part of 'theme.dart';
 
 class AppColor {
-  // TODO: define color
+  static Color primary = Color(0xFF001767);
+
+  static Color bgSecondary = Color(0xFFF1F4F9);
+
+  static Color success = Color.fromARGB(255, 3, 167, 33);
+  static Color danger = Color(0xFFFF463D);
+  static Color info = Color(0xFF2F80ED);
+
+  static Color textPrimary = Color(0xFF202327);
+  static Color textSecondary = Color(0xFF68778D);
 }
--- lib/presentation/pages/splash/splash_page.dart
+++ lib/presentation/pages/splash/splash_page.dart
@@ -1,6 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
+import '../../../common/theme/theme.dart';
+import '../../components/assets/assets.gen.dart';
+
 @RoutePage()
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -9,12 +12,113 @@ class SplashPage extends StatefulWidget {
   State<SplashPage> createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage> {
+class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
+  late AnimationController _fadeController;
+  late AnimationController _scaleController;
+  late AnimationController _pulseController;
+
+  late Animation<double> _fadeAnimation;
+  late Animation<double> _scaleAnimation;
+  late Animation<double> _pulseAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize animation controllers
+    _fadeController = AnimationController(
+      duration: Duration(milliseconds: 1500),
+      vsync: this,
+    );
+
+    _scaleController = AnimationController(
+      duration: Duration(milliseconds: 1200),
+      vsync: this,
+    );
+
+    _pulseController = AnimationController(
+      duration: Duration(milliseconds: 1000),
+      vsync: this,
+    );
+
+    // Initialize animations
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _fadeController, curve: Curves.easeInOut),
+    );
+
+    _scaleAnimation = Tween<double>(begin: 0.3, end: 1.0).animate(
+      CurvedAnimation(parent: _scaleController, curve: Curves.elasticOut),
+    );
+
+    _pulseAnimation = Tween<double>(begin: 1.0, end: 1.1).animate(
+      CurvedAnimation(parent: _pulseController, curve: Curves.easeInOut),
+    );
+
+    // Start animations
+    _startAnimations();
+
+    // Navigate to main screen after splash
+    _navigateToMainScreen();
+  }
+
+  void _startAnimations() {
+    _fadeController.forward();
+
+    Future.delayed(Duration(milliseconds: 300), () {
+      _scaleController.forward();
+    });
+
+    Future.delayed(Duration(milliseconds: 1500), () {
+      _pulseController.repeat(reverse: true);
+    });
+  }
+
+  void _navigateToMainScreen() {
+    Future.delayed(Duration(milliseconds: 3000), () {});
+  }
+
+  @override
+  void dispose() {
+    _fadeController.dispose();
+    _scaleController.dispose();
+    _pulseController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
-        child: Text("Splash Page"),
+        child: FadeTransition(
+          opacity: _fadeAnimation,
+          child: ScaleTransition(
+            scale: _scaleAnimation,
+            child: ScaleTransition(
+              scale: _pulseAnimation,
+              child: Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [AppColor.primary, Color(0xFF9575CD)],
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: AppColor.primary.withOpacity(0.3),
+                      blurRadius: 25,
+                      offset: Offset(0, 10),
+                    ),
+                  ],
+                ),
+                child: Assets.images.logo.image(),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
