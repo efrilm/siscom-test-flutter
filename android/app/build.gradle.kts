7619f014cc6541ce842080ef0556d8adaa1f3ae5
efrilm <efril.mulanda@gmail.com>
2025-08-29 09:17:45 +0700

    feat: logo, package name, permission

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.siscom_test_flutter"
+    namespace = "com.dev.siscom_test"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.siscom_test_flutter"
+        applicationId = "com.dev.siscom_test"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
